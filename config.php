dfb54b614f2203c44f48c9a199c8b6a3776a0127
Earl_of_Berkeley <harry26548@gmail.com>
2021-04-03 09:41:30 +1100

    SQL info changes

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', '1stmardiv');
+define('DB_SERVER', '199.188.200.231');
+define('DB_USERNAME', 'rstmgvqt_1stmardiv');
+define('DB_PASSWORD', '&gnHrcG3eJzGN7iQP8LmYG');
+define('DB_NAME', 'rstmgvqt_1stmardiv');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
