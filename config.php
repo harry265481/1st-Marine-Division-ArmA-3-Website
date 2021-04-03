c56c377d9e35d8eb143973a8879c9ca0792832f3
DESKTOP-5G5BE3B\luiso <luisosorio508@gmail.com>
2021-04-02 21:37:16 -0400

    no message

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
     define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'rstmgvqt_1stmardiv');
-    define('DB_PASSWORD', '&gnHrcG3eJzGN7iQP8LmYG');
-    define('DB_NAME', 'rstmgvqt_1stmardiv');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', '1stmardiv');
 
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
