b2335f89fcf4f3c91a95494d99d011256f174beb
Earl_of_Berkeley <harry26548@gmail.com>
2021-04-07 00:59:38 +1000

    er5ty6u

--- admin/session.php
+++ admin/session.php
@@ -2,15 +2,14 @@
 include_once '../config.php';
 session_start();
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: sign-in.php");
+    header("location: ../sign-in.php");
     exit;
 } else {
     include_once '../config.php';
     $levelresult = mysqli_query($link, "SELECT adminlevel FROM users WHERE id=" . $_SESSION['id']);
     $userrow = mysqli_fetch_row($levelresult);
     if($userrow[0] == 0) {
-        header("location: index.php");
+        header("location: ../index.php");
         exit;
     }
 }
-?>
