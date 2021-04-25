7bf4d0a5119d20593e6002dee578428d82f2119f
Earl_of_Berkeley <harry26548@gmail.com>
2021-04-26 05:13:22 +1000

    Small fix

--- admin/graduate.php
+++ admin/graduate.php
@@ -19,7 +19,7 @@ $shortname = substr($row[0], 0, 1) . ". " . $row[1];
                 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-dark">
                     <form method="post" action="processgraduate.php?id=<?php echo $_GET['id'] ?>">
                         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                            <h1 class="h2 text-light">Award - <?php echo $shortname ?></h1>
+                            <h1 class="h2 text-light">Qualification - <?php echo $shortname ?></h1>
                             <div class="btn-toolbar mb-2 mb-md-0"></div>
                         </div>
                         <div class="row g-2">
