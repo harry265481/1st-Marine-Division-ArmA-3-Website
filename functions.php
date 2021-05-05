c19de7587f4285a335b9d54f75c004f92cbc0ec3
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-05 19:05:00 +1000

    aesdfrg

--- functions.php
+++ functions.php
@@ -45,8 +45,8 @@
                 $rank = imagecreatefrompng($path . "images/uniform/armyranks/" . getMemberRankID($id) . ".png");
                 $rankx = 182 - (imagesx($rank) / 2);
                 $ranky = 720;
-                imagecopy($bg, $rank, $rankx, $ranky, imagesx($rank), imagesy($rank), imagesx($rank), imagesy($rank));
-                imagecopy($bg, $rank, imagesx($bg) - (imagesx($rank) - $rankx), $ranky, imagesx($rank), imagesy($rank), imagesx($rank), imagesy($rank));
+                imagecopy($bg, $rank, $rankx, $ranky, 0, 0, imagesx($rank), imagesy($rank));
+                imagecopy($bg, $rank, imagesx($bg) - (imagesx($rank) - $rankx), $ranky, 0, 0, imagesx($rank), imagesy($rank));
             } else {
                 $rank = imagecreatefrompng($path . "images/uniform/armyranks/" . getMemberRankID($id) . ".png");
                 $rankx = 182 - (imagesx($rank) / 2);
