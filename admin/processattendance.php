56599dc3f383c242c96014e54f804cd3ce4fee83
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-06 14:19:17 +1000

    mgjc

--- admin/processattendance.php
+++ admin/processattendance.php
@@ -4,7 +4,7 @@ $attendances = $_POST['attendance'];
 $event = $_POST['event'];
 foreach($attendances as $ID=>$attendance) {
     foreach($attendance as $curr) {
-        mysqli_query($link, "INSERT INTO attendance (memberID, opid, presence) VALUES (' . $ID . ', '" . $event . "', '" . $curr . "')");
+        mysqli_query($link, "INSERT INTO attendance (memberID, opid, presence) VALUES ('" . $ID . "', '" . $event . "', '" . $curr . "')");
     }
 }
 ?>
