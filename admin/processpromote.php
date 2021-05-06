a4a396bfda08c6b8811d12591e9ee231c200335f
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-07 07:21:43 +1000

    rtyj

--- admin/processpromote.php
+++ admin/processpromote.php
@@ -5,7 +5,7 @@ include_once '../config.php';
 $id = $_GET['id'];
 $rank = $_POST['rank'];
 $date = $_POST['date'];
-$usetoday = $_POST['today'];
+if(issset($_POST['today']) $usetoday = $_POST['today'];
 
 if($usetoday == "true") {
     $date = date("d-M-Y");
