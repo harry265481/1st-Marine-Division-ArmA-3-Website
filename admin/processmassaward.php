0fcecce5ddc1194d70959552b34c50030a443121
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-06 23:58:34 +1000

    sdfgrhjn

--- admin/processmassaward.php
+++ admin/processmassaward.php
@@ -1,7 +1,7 @@
 <?php
     var_dump($_POST);
     $members = $_POST['member'];
-    $curraward = $_POST['curraward']
+    $curraward = $_POST['curraward'];
     foreach($members as $member) {
         if(mysqli_query($link, "INSERT INTO attendance (memberID, awardID, awardDate) VALUES ('" . $member . "', '" . $curraward . "', '" . $curr . "')")) {
             echo mysqli_error($link);
