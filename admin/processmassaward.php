614f8f3e24dae0dc014c109dfcc5f062953c36e2
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-07 00:29:44 +1000

    ghcvn

--- admin/processmassaward.php
+++ admin/processmassaward.php
@@ -5,11 +5,9 @@
     $curraward = $_POST['curraward'];
     $date = $_POST['date'];
     foreach($members as $member) {
-        echo $member . " | " . $curraward . " | " . $date . " <br>";
         if(mysqli_query($link, "INSERT INTO awardrecord (memberID, awardID, awarddate) VALUES ('" . $member . "', '" . $curraward . "', '" . $date . "')")) {
             echo mysqli_error($link);
         } else {
-            echo mysqli_error($link);
             //header("Location: members.php");
         }
     }
