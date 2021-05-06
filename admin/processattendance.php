1f4832dc7b286dd3fe00ca7504d5e8c2b6a6e5d0
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-06 14:13:05 +1000

    gfaed

--- admin/processattendance.php
+++ admin/processattendance.php
@@ -1,9 +1,10 @@
 <?php
 include_once '../config.php';
 $attendances = $_POST['attendance'];
+$event = $_POST['event'];
 foreach($attendances as $ID=>$attendance) {
     foreach($attendance as $curr) {
-        mysqli_query($link, "INSERT INTO attendance () VALUES ()");
+        mysqli_query($link, "INSERT INTO attendance (memberID, opid, presence) VALUES (' . $ID . ', '" . $event . "', '" . $curr . "')");
     }
 }
 ?>
