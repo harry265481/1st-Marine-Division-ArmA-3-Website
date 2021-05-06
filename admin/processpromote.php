54298f93e59111298060269f699f619ea9ae9c0b
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-07 07:30:20 +1000

    4w35

--- admin/processpromote.php
+++ admin/processpromote.php
@@ -20,6 +20,5 @@ mysqli_query($link, "UPDATE personnel SET rank=" . $rank . " WHERE ID=" . $id);
 
 //Add record to record table
 mysqli_query($link, "INSERT INTO records (memberID, recordType, promorank, recorddate) VALUES ('" . $id . "', '2', '" . $rank . "', '" . $date . "')");
-mysqli_error($link);
-//header("Location: members.php");
+header("Location: members.php");
 ?>
