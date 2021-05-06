975ac0447c01a04ab076aee523ad1b8b7613c620
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-06 12:36:20 +1000

    qerfg

--- admin/processattendance.php
+++ admin/processattendance.php
@@ -3,6 +3,8 @@
 var_dump($_POST);
 $attendances = $_POST['attendance'];
 foreach($attendances as $attendance) {
-    echo $attendance[0] . "<br>";
+    foreach($attendance as $curr) {
+        echo $curr . "<br>";
+    }
 }
 ?>
