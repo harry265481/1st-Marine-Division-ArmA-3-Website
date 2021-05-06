c07a3a7a4127179c08d8b7ac8c60b4d786a90747
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-06 14:20:58 +1000

    saewfe

--- admin/processattendance.php
+++ admin/processattendance.php
@@ -4,7 +4,7 @@ $attendances = $_POST['attendance'];
 $event = $_POST['event'];
 foreach($attendances as $ID=>$attendance) {
     foreach($attendance as $curr) {
-        if(mysqli_query($link, "INSERT INTO attendance (memberID, opid, presence) VALUES ('" . $ID . "', '" . $event . "', '" . $curr . "')"))
+        if(mysqli_query($link, "INSERT INTO attendance (memberID, opid, presence) VALUES ('" . $ID . "', '" . $event . "', '" . $curr . "')")) {
             echo mysqli_error($link);
         } else {
             header("Location: members.php");
