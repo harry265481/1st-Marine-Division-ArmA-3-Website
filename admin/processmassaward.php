1fb71b49a3fc1d352ac84a71d1c01c9958deb847
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-07 00:08:37 +1000

    rtyj

--- admin/processmassaward.php
+++ admin/processmassaward.php
@@ -4,7 +4,7 @@
     $curraward = $_POST['curraward'];
     $date = $_POST['date'];
     foreach($members as $member) {
-        if(mysqli_query($link, "INSERT INTO awardrecord (memberID, awardID, awarddate) VALUES ('" . $member . "', '" . $curraward . "', '" . $curr . "')")) {
+        if(mysqli_query($link, "INSERT INTO awardrecord (memberID, awardID, awarddate) VALUES ('" . $member . "', '" . $curraward . "', '" . $date . "')")) {
             echo mysqli_error($link);
         } else {
             header("Location: members.php");
