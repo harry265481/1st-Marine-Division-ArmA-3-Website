76fd4a502e6f3de40c42811252a7f65fb00c27bc
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-06 12:38:22 +1000

    tyuj

--- admin/processattendance.php
+++ admin/processattendance.php
@@ -4,7 +4,7 @@ var_dump($_POST);
 $attendances = $_POST['attendance'];
 foreach($attendances as $attendance) {
     foreach($attendance as $curr) {
-        echo $curr . "<br>";
+        echo $curr . "<br>"; 
     }
 }
 ?>
