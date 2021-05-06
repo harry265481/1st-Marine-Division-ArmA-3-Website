1ab52efa15fe2eebbecc9102ffda144b409d2774
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-06 12:42:16 +1000

    hstegr

--- admin/processattendance.php
+++ admin/processattendance.php
@@ -2,8 +2,8 @@
 
 var_dump($_POST);
 $attendances = $_POST['attendance'];
-foreach($attendances as $attendance) {
-    foreach($attendance as $ID=>$curr) {
+foreach($attendances as $ID=>$attendance) {
+    foreach($attendance as $curr) {
         echo $ID . " " . $curr . "<br>";
     }
 }
