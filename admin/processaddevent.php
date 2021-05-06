d452aaeae8d7a561f0aa75d4e30c4e7b015c449e
Earl_of_Berkeley <harry26548@gmail.com>
2021-05-06 13:48:32 +1000

    yfguj

--- admin/processaddevent.php
+++ admin/processaddevent.php
@@ -4,7 +4,7 @@
     $title = $_POST['title'];
 
     //Add record to record table
-    if(mysqli_query($link, "INSERT INTO events (eventitle, eventdate) VALUES ('" . $title . "', '" . $date . "')") == false) {
+    if(mysqli_query($link, "INSERT INTO events (eventtitle, eventdate) VALUES ('" . $title . "', '" . $date . "')") == false) {
         echo mysqli_error($link);
     } else {
         header("Location: events.php");
