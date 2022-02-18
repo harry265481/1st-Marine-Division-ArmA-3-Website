ad89108d7716d91b9682ba06462509813bd9eefc
Earl_of_Berkeley <harry26548@gmail.com>
2022-02-19 10:19:43 +1100

    Application code

--- submitapplication.php
+++ submitapplication.php
@@ -6,13 +6,12 @@
     $dob = $_POST['dob'];
     $referrer = $_POST['referrer'];
     $steamid = $_POST['steamid'];
-    $platoon = $_POST['platoon'];
     $career = $_POST['career'];
     $veteran = $_POST['veteran'];
     $otherunits = $_POST['otherunits'];
     $method = $_POST['referralselect'];
 
-    $sql = "INSERT INTO applications (firstname, surname, discord, dob, referrer, steamid, platoon, career, veteran, otherunits, appstatus, method) VALUES ('" . $firstname . "', '" . $surname . "', '" . $discord . "', '" . $dob . "', '" . $referrer . "', '" . $steamid . "', '" . $platoon . "', '" . $career . "', '" . $veteran . "', '" . $otherunits . "', 0, '" . $method . "')";
+    $sql = "INSERT INTO applications (firstname, surname, discord, dob, referrer, steamid, career, veteran, otherunits, appstatus, method) VALUES ('" . $firstname . "', '" . $surname . "', '" . $discord . "', '" . $dob . "', '" . $referrer . "', '" . $steamid . "', '" . $career . "', '" . $veteran . "', '" . $otherunits . "', 0, '" . $method . "')";
     if (mysqli_query($link, $sql)) {
         header( "refresh:5;url=index.php" );
         echo 'You\'ll be redirected in about 5 secs. If not, click <a href="index.php">here</a>.';
