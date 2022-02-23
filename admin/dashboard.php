1ea78eb015a2b2dd51429401a18ab23a62c86892
Earl_of_Berkeley <harry26548@gmail.com>
2022-02-23 22:50:23 +1100

    coloring

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -16,7 +16,7 @@
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                         <h1 class="h2 text-light">Dashboard</h1>
                     </div>
-                    <div class="container">
+                    <div class="container text-light">
                         <div class="row">
                             <div class="col-md-3">
                                 <h5>Total Unit Members: <?php echo getTotalUnitMembers() ?></h5>
