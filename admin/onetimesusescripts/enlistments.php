d9c611c2c0f696e823ee36c363e248869f7a827b
Earl_of_Berkeley <harry26548@gmail.com>
2022-02-24 09:36:43 +1100

    There was an attempt

--- admin/onetimesusescripts/enlistments.php
+++ admin/onetimesusescripts/enlistments.php
@@ -3,6 +3,6 @@ include_once '../../config.php';
 $members = "SELECT ID, DOE FROM personnel";
 $members = mysqli_query($link, $members);
 foreach($members as $member) {
-    mysqli_query($link, "INSERT INTO records (memberID, recordType, recorddate) VALUES ('" . $member[0] . "', 0, '" . $member[1] . "')");
+    mysqli_query($link, "INSERT INTO records (memberID, recordType, recorddate) VALUES ('" . $member['ID'] . "', 0, '" . $member['DOE'] . "')");
 }
 ?>
