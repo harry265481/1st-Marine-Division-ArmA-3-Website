fed4a73164ebae44b83a0f1d16a5dfacf2c0e0be
Earl_of_Berkeley <harry26548@gmail.com>
2022-02-23 22:34:08 +1100

    Starting dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -15,6 +15,8 @@
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                         <h1 class="h2 text-light">Dashboard</h1>
                     </div>
+                    <div class="container">
+                    </div>
                 </main>
             </div>
         </div>
