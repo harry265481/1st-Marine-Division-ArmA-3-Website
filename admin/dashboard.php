4d299bb6c53019a12a83ef5fb134a51a95acebe0
Earl_of_Berkeley <harry26548@gmail.com>
2022-02-25 10:24:00 +1100

    Formatting

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -25,6 +25,8 @@
                                 <h5></h5>
                                 <h5></h5>
                             </div>
+                            <div class="col-md-3"></div>
+                            <div class="col-md-3"></div>
                         </div>
                     </div>
                 </main>
