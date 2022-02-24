a11e32bee8149eb41d285963a661c087892e825f
Earl_of_Berkeley <harry26548@gmail.com>
2022-02-25 10:32:51 +1100

    Removed enlistment sctript

--- admin/onetimesusescripts/enlistments.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once '../../config.php';
-$members = "SELECT ID, DOE FROM personnel";
-$members = mysqli_query($link, $members);
-foreach($members as $member) {
-    mysqli_query($link, "INSERT INTO records (memberID, recordType, recorddate) VALUES ('" . $member['ID'] . "', 0, '" . $member['DOE'] . "')");
-}
-?>
