68f942b7f896eb5b6dd503288a68cc4c7566a9fe
Earl_of_Berkeley <harry26548@gmail.com>
2022-03-03 03:55:36 +1100

    Working on attendance I promise

--- admin/attendance.php
+++ admin/attendance.php
@@ -15,41 +15,48 @@ include_once '../functions.php';
             <div class="row">
                 <?php include_once 'nav.php'; ?>
                 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-dark">
-                    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                        <h1 class="h2 text-light">Attendance</h1>
-                        <h1></h1>
-                        <div class="btn-toolbar mb-2 mb-md-0"></div>
-                    </div>
-                    <div class="table-responsive">
-                        <div class="btn-toolbar mb-2 mb-md-0\"></div>
-                        <table class="table table-dark table-striped align-middle table-sm">
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th></th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                    <?php
-                        $battunits = mysqli_query($link, "SELECT * FROM units WHERE parents='' ORDER BY unitorder asc");
-                        foreach($battunits as $batt) {
-                                buildAttendanceRow($batt['ID'], "../");
-                            $compunits = mysqli_query($link, "SELECT * FROM units WHERE parents=" . $batt['ID'] . " ORDER BY unitorder asc");
-                            foreach($compunits as $comp) {
-                                buildAttendanceRow($comp['ID'], "../");
-                                $platunits = mysqli_query($link, "SELECT * FROM units WHERE parents=" . $comp['ID'] . " ORDER BY unitorder asc");
-                                foreach($platunits as $plat) {
-                                    buildAttendanceRow($plat['ID'], "../");
-                                    $squadunits = mysqli_query($link, "SELECT * FROM units WHERE parents=" . $plat['ID'] . " ORDER BY unitorder asc");
-                                    foreach($squadunits as $squad) {
-                                        buildAttendanceRow($squad['ID'], "../");
+                    <form method="post" action="processattendance.php">
+                        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                            <h1 class="h2 text-light">Attendance</h1>
+                            <h1></h1>
+                            <div class="btn-toolbar mb-2 mb-md-0"></div>
+                        </div>
+                        <div class="table-responsive">
+                            <div class="btn-toolbar mb-2 mb-md-0\"></div>
+                            <table class="table table-dark table-striped align-middle table-sm">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                    $battunits = mysqli_query($link, "SELECT * FROM units WHERE parents='' ORDER BY unitorder asc");
+                                    foreach($battunits as $batt) {
+                                            buildAttendanceRow($batt['ID'], "../");
+                                        $compunits = mysqli_query($link, "SELECT * FROM units WHERE parents=" . $batt['ID'] . " ORDER BY unitorder asc");
+                                        foreach($compunits as $comp) {
+                                            buildAttendanceRow($comp['ID'], "../");
+                                            $platunits = mysqli_query($link, "SELECT * FROM units WHERE parents=" . $comp['ID'] . " ORDER BY unitorder asc");
+                                            foreach($platunits as $plat) {
+                                                buildAttendanceRow($plat['ID'], "../");
+                                                $squadunits = mysqli_query($link, "SELECT * FROM units WHERE parents=" . $plat['ID'] . "  ORDER BY unitorder asc");
+                                                foreach($squadunits as $squad) {
+                                                    buildAttendanceRow($squad['ID'], "../");
+                                                }
+                                            }
+                                        }
                                     }
-                                }
-                            }
-                        }
-                    ?>
-                    </div>
+                                ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="col-12">
+                            <button class="btn btn-primary" type="submit" name="submit">Submit</button>
+                        </div>
+                    </form>
                 </main>
             </div>
         </div>
--- admin/processattendance.php
+++ admin/processattendance.php
@@ -2,14 +2,28 @@
 
 include_once '../config.php';
 
+echo "<table>";
+    foreach ($_POST as $key => $value) {
+        echo "<tr>";
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+        echo "<td>";
+        echo $value;
+        echo "</td>";
+        echo "</tr>";
+    }
+echo "</table>";
+/*
 $id = $_GET['id'];
 $rank = $_POST['rank'];
 $date = $_POST['date'];
-
+*/
 //Update personnel record
-mysqli_query($link, "UPDATE personnel SET rank=" . $rank . " WHERE ID=" . $id);
+//mysqli_query($link, "UPDATE personnel SET rank=" . $rank . " WHERE ID=" . $id);
 
 //Add record to record table
-mysqli_query($link, "INSERT INTO records (memberID, recordType, promorank, recorddate) VALUES (" . $id . ", 2," . $rank . ", '" . $date . "')");
-header("Location: members.php");
+//mysqli_query($link, "INSERT INTO records (memberID, recordType, promorank, recorddate) VALUES (" . $id . ", 2," . $rank . ", '" . $date . "')");
+
+//header("Location: members.php");
 ?>
