ad9c935ebfd04b5dbd22592abd58c29861d55a09
Earl_of_Berkeley <harry26548@gmail.com>
2022-03-03 04:02:32 +1100

    debugging attendance

--- admin/processattendance.php
+++ admin/processattendance.php
@@ -14,6 +14,19 @@ echo "<table>";
         echo "</tr>";
     }
 echo "</table>";
+
+echo "<table>";
+    foreach ($_POST['attendance'] as $key => $value) {
+        echo "<tr>";
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+        echo "<td>";
+        echo $value;
+        echo "</td>";
+        echo "</tr>";
+    }
+echo "</table>";
 /*
 $id = $_GET['id'];
 $rank = $_POST['rank'];
